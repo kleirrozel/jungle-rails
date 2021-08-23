a40949d4f8305369d4f4abe4359ca3167e6c3da3
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-23 13:26:09 -0400

    Refactored rspec test to be more condensed based on mentor feedback

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,68 +8,36 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
     it 'should only save the product when the name, price, quantity, and category fields have been filled out' do
-      @category = Category.create 
-      @category.name = 'Pet Supplies'
-
-      @product = Product.new
-      @product.name = 'Chew Toy'
-      @product.price = 45.00
-      @product.quantity = 12
-      @product.category = @category
+      @category = Category.new(name: 'Pet Supplies')
+      @product = Product.new(name: 'Carrot Chew Toy', price: 45.00, quantity: 12, category: @category)
 
       expect(@product).to be_valid
-
-      # @product!save
-      # expect(@product.id).to be_present
     end
 
     it 'should not save when the name field is left empty or blank' do
-      @category = Category.create 
-      @category.name = 'Pet Supplies'
+      @category = Category.new(name: 'Pet Supplies')
+      @product = Product.new(name: nil, price: 45.00, quantity: 12, category: @category)
 
-      @product = Product.new
-      @product.name = nil
-      @product.price = 45.00
-      @product.quantity = 12
-      @product.category = @category
-      
       expect(@product.errors[:name]).to be_empty
     end
 
     it 'should not save when the price field is left empty or blank' do
-      @category = Category.create 
-      @category.name = 'Pet Supplies'
+      @category = Category.new(name: 'Pet Supplies')
+      @product = Product.new(name: 'Carrot Chew Toy', price: nil, quantity: 12, category: @category)
 
-      @product = Product.new
-      @product.name = 'Chew Toy'
-      @product.price = nil
-      @product.quantity = 12
-      @product.category = @category
-      
       expect(@product.errors[:price]).to be_empty
     end
 
     it 'should not save when the quantity field is left empty or blank' do
-      @category = Category.create 
-      @category.name = 'Pet Supplies'
-
-      @product = Product.new
-      @product.name = 'Chew Toy'
-      @product.price = 45.00
-      @product.quantity = nil
-      @product.category = @category
+      @category = Category.new(name: 'Pet Supplies')
+      @product = Product.new(name: 'Carrot Chew Toy', price: 45.00, quantity: nil, category: @category)
       
       expect(@product.errors[:quantity]).to be_empty
     end
 
     it 'should not save when the quantity field is left empty or blank' do
       @category = nil
-
-      @product = Product.new
-      @product.name = 'Chew Toy'
-      @product.price = 45.00
-      @product.quantity = 12
-      @product.category = @category
+      @product = Product.new(name: 'Carrot Chew Toy', price: 45.00, quantity: nil, category: @category)
       
       expect(@product.errors[:category]).to be_empty
     end
