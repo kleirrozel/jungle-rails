ac592006bf87221cc8c5f1b4858c9c6393314fd0
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-23 20:55:32 -0400

    Product Details/Description page capybara

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -19,13 +19,13 @@ RSpec.feature "Visitor navigates to product details page", type: :feature, js: t
 
   scenario "They see the product description page" do
     visit root_path
-    first('article.product').find_link('Details').click
+    first('article.product').find_link('Details')click 'Details'
 
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
+    save_and_open_screenshot
 
     # puts page.html
 
-    expect(page).to have_css 'article.product-detail', count: 1
+    expect(page).to have_css 'article.product'
   end
 end
