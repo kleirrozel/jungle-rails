56fbab1260c5e03c58e569984b0f80c24f77f94b
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-23 21:10:48 -0400

    Added sleep (3) to take correct screenshot and corrected expect

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -19,13 +19,14 @@ RSpec.feature "Visitor navigates to product details page", type: :feature, js: t
 
   scenario "They see the product description page" do
     visit root_path
-    first('article.product').find_link('Details').click 'Details'
+    first('article.product').click_on('Details')
 
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
+    sleep(3)
+    save_and_open_screenshot
 
     # puts page.html
 
-    expect(page).to have_css 'article.product'
+    expect(page).to have_css 'article.product-detail', count: 1
   end
 end
